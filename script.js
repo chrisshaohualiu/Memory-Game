2a78f19df064bd8e16228e6e00155912ec039a06
chris <chrisshaohualiu@gmail.com>
2020-08-06 10:33:44 -0400

    chris' first commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let gameBoard = [];
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+let randomArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
