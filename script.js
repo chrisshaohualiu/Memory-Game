74d4c6d3584a1652caf5221a58303e6011364ed6
Stephanie <stephanienicolewheatley@gmail.com>
2020-08-06 11:02:19 -0400

    mehh

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function shuffle(a) {
 }
 
 let randomArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+
+shuffle.apply(randomArray);
+
+for (let i = 0; i < 16; i++) {
+  let place = {
+    symbol: randomArray[i],
+  };
+  gameBoard.push(place);
+}
